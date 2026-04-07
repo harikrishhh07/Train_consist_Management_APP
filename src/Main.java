ab3b514a4aafa49117872c0509510d2ac839baa3
harikrishhh07 <harielamvazuthi@gmail.com>
2026-04-07 09:14:34 +0530

    UC19: Binary Search for Bogie ID (Optimized Searching)

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,41 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("=== UC18: Search Bogie ID using Linear Search ===");
+        System.out.println("=== UC19: Binary Search for Bogie ID ===");
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
         String searchId = "BG309";
 
-        System.out.println("Available bogie IDs:");
-        for (String id : bogieIds) {
-            System.out.print(id + " ");
-        }
+        System.out.println("Sorted bogie IDs:");
+        System.out.println(Arrays.toString(bogieIds));
 
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchId)) {
+        while (low <= high) {
+
+            int mid = (low + high) / 2;
+
+            int comparison = searchId.compareTo(bogieIds[mid]);
+
+            if (comparison == 0) {
                 found = true;
+                System.out.println("Bogie ID found at index: " + mid);
                 break;
+            } else if (comparison > 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
-        if (found) {
-            System.out.println("\nBogie ID found: " + searchId);
-        } else {
-            System.out.println("\nBogie ID not found: " + searchId);
+        if (!found) {
+            System.out.println("Bogie ID not found: " + searchId);
         }
     }
 }
