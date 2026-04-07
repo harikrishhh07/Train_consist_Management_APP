fe110eb5eecd9467ce717a41302f864666fa822c
harikrishhh07 <harielamvazuthi@gmail.com>
2026-04-07 09:10:38 +0530

    UC17: Sort Bogie Names using Arrays.sort

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("=== UC16: Sort Bogie Capacities using Bubble Sort ===");
+        System.out.println("=== UC17: Sort Bogie Names using Arrays.sort ===");
 
-        int[] capacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {"Sleeper", "AC Chair", "First Class", "General", "Luxury"};
 
-        System.out.println("Original capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(bogieNames));
 
-        for (int i = 0; i < capacities.length - 1; i++) {
-            for (int j = 0; j < capacities.length - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
+        Arrays.sort(bogieNames);
 
-        System.out.println("\nSorted capacities:");
-        for (int c : capacities) {
-            System.out.print(c + " ");
-        }
+        System.out.println("After sorting:");
+        System.out.println(Arrays.toString(bogieNames));
     }
 }
